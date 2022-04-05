95f312c1bd0d0bc6e4ab8913609fce7c7c2f2109
Emily Griggs <egriggs4@gmail.com>
2022-04-04 23:51:53 -0500

    createBatches.hpp started git push

---
+++ createBatches.hpp
@@ -0,0 +1,42 @@
+/***************************************************************
+  Student Name: Emily Griggs
+  File Name: createBatches.hpp
+  Project 4 - Monte Carlo
+
+Class to create chip batches using input file data
+***************************************************************/
+#ifndef CREATEBATCHES_HPP
+#define CREATEBATCHES_HPP
+
+#include <iostream>
+#include <fstream>
+
+class CreateBatches{
+    private:   
+        std::string fileName1 = "t1.txt";
+        std::string fileName2 = "t2.txt";
+        std::string fileName3 = "t3.txt";
+        std::string fileName4 = "t4.txt";
+        std::string batchFile = "";
+        std::ifstream infile;
+        std::ifstream outfile;
+        int numBatches = 0; //number of batches of items
+        int itemsPerBatch = 0; // number of items in each batch
+        int percentBadBatches = 0; // percent of batches containing bad items
+        int percentBadItems = 0; // percent of items that are bad in a bed set
+        int numSampled = 0; // number of items sampled from each set
+        int countBad = 0;
+        int maxBad = 0; //max number of bad items in a bad set
+        int minBad = 0; // min number of bad items in a bad set
+        float averageNumBad = 0; // average number of bad items in a bad set
+    public:
+        CreateBatches();
+        bool openFile(std::string fileName);
+        void readFile();
+        void writeFiles();
+        float getAverageNumBad();
+        int getMaxBad();
+        int getMinBad();
+};
+
+#endif //createBatches.hpp
